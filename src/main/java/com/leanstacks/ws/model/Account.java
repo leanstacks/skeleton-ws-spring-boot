611a36c33aa4ec1611938370ab4bf30ea44eaa08
Matt Warman <warmanmr@gmail.com>
2015-02-15 11:45:03 -0500

    Initial addition of Spring Security.  Initialize HSQLDB from scripts.

---
+++ src/main/java/com/leanstacks/ws/model/Account.java
@@ -0,0 +1,121 @@
+package com.leanstacks.ws.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private boolean enabled = true;
+
+    @NotNull
+    private boolean credentialsexpired = false;
+
+    @NotNull
+    private boolean expired = false;
+
+    @NotNull
+    private boolean locked = false;
+
+    @ManyToMany(
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "AccountRole",
+            joinColumns = @JoinColumn(
+                    name = "accountId",
+                    referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "roleId",
+                    referencedColumnName = "id"))
+    private Set<Role> roles;
+
+    public Account() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isCredentialsexpired() {
+        return credentialsexpired;
+    }
+
+    public void setCredentialsexpired(boolean credentialsexpired) {
+        this.credentialsexpired = credentialsexpired;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
