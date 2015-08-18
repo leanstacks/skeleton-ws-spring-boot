0cf5607d4e78f6aad718fdd23729d2442a396796
Matthew Warman <warmanmr@gmail.com>
2015-08-18 14:16:23 -0400

    JavaDoc

--- src/test/java/com/leanstacks/ws/util/BCryptPasswordEncoderUtil.java
+++ src/test/java/com/leanstacks/ws/util/BCryptPasswordEncoderUtil.java
@@ -4,13 +4,34 @@ import java.util.Scanner;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * The BCryptPasswordEncoderUtil class assists engineers during application
+ * construction. It is not intended for use in a 'live' application.
+ * 
+ * The class uses a BCryptPasswordEncoder to encrypt clear text values using
+ * it's native hashing algorithm. This utility may be used to create encrypted
+ * password values in a database initialization script used for unit testing or
+ * local machine development.
+ * 
+ * @author Matt Warman
+ *
+ */
 public class BCryptPasswordEncoderUtil {
 
+    /**
+     * Uses a BCryptPasswordEncoder to hash the clear text value.
+     * @param clearText A String of clear text to be encrypted.
+     * @return The encrypted (hashed) value.
+     */
     public static String encode(String clearText) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder.encode(clearText);
     }
 
+    /**
+     * Facilitates gathering user input and invoking the class behavior.
+     * @param args An array of command line input values. (not used)
+     */
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
