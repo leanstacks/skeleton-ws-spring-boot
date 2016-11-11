84dd25a4227d09b4741305afa6a9874b95f8f6a3
Matt Warman <warmanmr@gmail.com>
2016-11-11 08:08:22 -0500

    Upgraded Swagger to version 2.6.1

--- src/main/java/com/leanstacks/ws/ApiDocsConfiguration.java
+++ src/main/java/com/leanstacks/ws/ApiDocsConfiguration.java
@@ -9,6 +9,7 @@ import com.google.common.base.Predicate;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,9 +29,43 @@ public class ApiDocsConfiguration {
      */
     public static final String PROJECT_VERSION = "1.6.3";
     /**
-     * The project contact information.
+     * The project contact name.
      */
-    public static final String PROJECT_CONTACT = "LeanStacks.com";
+    public static final String PROJECT_CONTACT_NAME = "LeanStacks.com";
+    /**
+     * The project contact URL.
+     */
+    public static final String PROJECT_CONTACT_URL = "http://www.leanstacks.com/";
+
+    /**
+     * Create a Contact class to be used by Springfox's Swagger API Documentation framework.
+     * 
+     * @return A Contact instance.
+     */
+    private Contact contact() {
+        return new Contact(PROJECT_CONTACT_NAME, PROJECT_CONTACT_URL, null);
+    }
+
+    /**
+     * Create an ApiInfo class to be used by Springfox's Swagger API Documentation framework.
+     * 
+     * @return An ApiInfo instance.
+     */
+    private ApiInfo apiInfo() {
+
+        // @formatter:off
+        final ApiInfo apiInfo = 
+            new ApiInfoBuilder()
+                .title("Project Skeleton for Spring Boot Web Services")
+                .description("The Spring Boot web services starter project provides a foundation "
+                        + "to rapidly construct a RESTful web services application.")
+                .contact(contact())
+                .version(PROJECT_VERSION)
+                .build();
+        // @formatter:on
+
+        return apiInfo;
+    }
 
     /**
      * Create a Docket class to be used by Springfox's Swagger API Documentation framework. See
@@ -42,12 +77,14 @@ public class ApiDocsConfiguration {
     public Docket docket() {
         final Predicate<String> paths = PathSelectors.ant("/api/**");
 
-        final ApiInfo apiInfo = new ApiInfoBuilder().title("Project Skeleton for Spring Boot Web Services")
-                .description("The Spring Boot web services starter project provides a foundation "
-                        + "to rapidly construct a RESTful web services application.")
-                .contact(PROJECT_CONTACT).version(PROJECT_VERSION).build();
-
-        final Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select().paths(paths).build();
+        // @formatter:off
+        final Docket docket = 
+            new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                    .paths(paths)
+                .build();
+        // @formatter:on
 
         return docket;
     }
