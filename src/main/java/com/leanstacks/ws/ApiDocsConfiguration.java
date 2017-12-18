137a305f71ce6c334ed995b9d7952725dfc2177a
Matt Warman <warmanmr@gmail.com>
2017-12-18 12:43:50 -0500

    Fixes #33. Update project metadata.

--- src/main/java/com/leanstacks/ws/ApiDocsConfiguration.java
+++ src/main/java/com/leanstacks/ws/ApiDocsConfiguration.java
@@ -27,7 +27,7 @@ public class ApiDocsConfiguration {
     /**
      * The project version.
      */
-    public static final String PROJECT_VERSION = "1.7.5";
+    public static final String PROJECT_VERSION = "1.7.6";
     /**
      * The project contact name.
      */
