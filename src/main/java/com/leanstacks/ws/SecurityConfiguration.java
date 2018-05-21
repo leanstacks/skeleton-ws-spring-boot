1a1f80a13fd814dc4b0a7043c4c9d9f38aabc7d9
Matt Warman <warmanmr@gmail.com>
2018-05-21 14:28:39 -0400

    See #38. Show health endpoint detail when authorized.

--- src/main/java/com/leanstacks/ws/SecurityConfiguration.java
+++ src/main/java/com/leanstacks/ws/SecurityConfiguration.java
@@ -1,11 +1,11 @@
 package com.leanstacks.ws;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -119,10 +119,10 @@ public class SecurityConfiguration {
             
             http
             .csrf().disable()
-            .antMatcher("/actuators/**")
+            .requestMatcher(EndpointRequest.toAnyEndpoint())
               .authorizeRequests()
                 // Permit access to health check
-                .antMatchers(HttpMethod.GET, "/actuators/health").permitAll()
+                .requestMatchers(EndpointRequest.to("health")).permitAll()
                 // Require authorization for everthing else
                 .anyRequest().hasRole("SYSADMIN")
             .and()
