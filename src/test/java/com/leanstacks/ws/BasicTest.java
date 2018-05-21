0c815ec4f17e5a414ee487b0ec88f7654ea875f6
Matt Warman <warmanmr@gmail.com>
2018-05-21 14:00:46 -0400

    See #38. Create test annotations. Update tests.

---
+++ src/test/java/com/leanstacks/ws/BasicTest.java
@@ -0,0 +1,17 @@
+package com.leanstacks.ws;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@ActiveProfiles("hsqldb")
+public @interface BasicTest {
+
+}
